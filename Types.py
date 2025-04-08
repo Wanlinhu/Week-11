5f1ded5586244f456393286684e0a0d6150f6ba5
Wanlin Huang <wanlinhuang@Wanlins-MacBook-Pro.local>
2025-04-08 19:02:54 -0400

    Level 4 Complete

---
+++ Types.py
@@ -0,0 +1,8 @@
+a = 3 #I am a integer
+b = 4.0 #I am a float
+C = True #I am a boolean
+D = "Hey!" #I am a string
+f = none #I am a NoneType
+age = 32 #I am a integer
+name = "Wanlin Huang" #I am a string
+Instrument= "Piano" #I am a string
